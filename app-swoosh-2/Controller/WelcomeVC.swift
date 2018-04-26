955171cdcf36914c1867a5480ef30107dd85ea54
Pierre Baseley <pierrebaseley@Pierres-MacBook-Pro.local>
2018-04-26 21:34:36 +0200

    Good habits

--- app-swoosh-2/Controller/WelcomeVC.swift
+++ app-swoosh-2/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var Swoosh: UIImageView!
     @IBOutlet weak var BGImage: UIImageView!
